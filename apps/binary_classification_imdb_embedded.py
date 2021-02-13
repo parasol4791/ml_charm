ef7934a887cf04fad2d3e4e86b73dc36ab59daf3
dvk11 <koulikovd@gmail.com>
2021-02-12 23:25:47 -0500

    Experimenting with pre-trained & self-trained embeddings. Top validation accuracy is stuck at 87-89%, as in a model with one-hot word encodings. But the model with embeddings is slower, due to many more trainable parameters.

--- apps/binary_classification_imdb_embedded.py
+++ apps/binary_classification_imdb_embedded.py
@@ -8,7 +8,7 @@ import numpy as np
 import logging
 import time
 from keras.models import Sequential
-from keras.layers import Embedding, Flatten, Dense
+from keras.layers import Embedding, Flatten, Dense, Dropout
 from utils.data_preprocessing import shuffle, embeddings_GloVe
 from keras.preprocessing.sequence import pad_sequences
 from keras.preprocessing.text import Tokenizer
@@ -27,11 +27,12 @@ def convert_zero_one(val):
 
 
 # Training params
-maxlen = 100  # use only that many first words in each review
+maxlen = 10000  # use only that many first words in each review
 max_words = 10000  # Only that many top words are used from the dataset
-embeddings_dim = 100  # dimensionality of embedding coefficients
-train_sz = 200
-valid_sz = 10000
+embeddings_dim = 8  # dimensionality of embedding coefficients
+train_sz = 20000
+valid_sz = 20000
+use_pretrained_emb = False
 
 t0 = time.time()
 data_dir = os.environ['DATASETS_DIR']
@@ -69,17 +70,18 @@ print('Test: ', x_test.shape)
 word_index = tokenizer.word_index
 print('Found {} unique tokens'.format(len(word_index)))
 
-word_vectors = embeddings_GloVe(dim=embeddings_dim)  # 6B tokens
-print(len(word_vectors))
+if use_pretrained_emb:
+    word_vectors = embeddings_GloVe(dim=embeddings_dim)  # 6B tokens
+    print(len(word_vectors))
 
-# Build an embeddings matrix (word indes : embedding coefficients)
-word_index = tokenizer.word_index
-embed_matrix = np.zeros((max_words, embeddings_dim))
-for word, i in word_index.items():
-    if i < max_words:
-        coeffs = word_vectors.get(word)  # attemt getting pretrained embedding coefficients for a word
-        if coeffs is not None:
-            embed_matrix[i] = coeffs
+    # Build an embeddings matrix (word indes : embedding coefficients)
+    word_index = tokenizer.word_index
+    embed_matrix = np.zeros((max_words, embeddings_dim))
+    for word, i in word_index.items():
+        if i < max_words:
+            coeffs = word_vectors.get(word)  # attemt getting pretrained embedding coefficients for a word
+            if coeffs is not None:
+                embed_matrix[i] = coeffs
 
 t1 = time.time()
 print('It took {} sec to load data'.format(t1-t0))
@@ -90,12 +92,14 @@ model = Sequential()
 model.add(Embedding(max_words, embeddings_dim, input_length=maxlen))
 model.add(Flatten())
 model.add(Dense(32, activation='relu'))
+model.add(Dropout(0.5))
 model.add(Dense(1, activation='sigmoid'))
 logging.info(model.summary())
 
 # Load pretrained embedded word vectors to Embedded layer (the first layer in the model)
-model.layers[0].set_weights([embed_matrix])
-model.layers[0].trainable = False
+if use_pretrained_emb:
+    model.layers[0].set_weights([embed_matrix])
+    model.layers[0].trainable = False
 
 model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
 history = model.fit(x_train, y_train, epochs=10, batch_size=32, validation_data=(x_valid, y_valid))
@@ -112,8 +116,35 @@ plot_accuracy_loss(
 
 # Results
 """
-GPU, ptetrained GloVe embeddings, 200 samples. Severe overfitting
+GPU, self-trained embeddings, 200 train samples, severy overfitting
+Epoch 10/10
+1/1 [==============================] - 0s 292ms/step - loss: 0.0310 - acc: 1.0000 - val_loss: 0.7087 - val_acc: 0.5154
+It took 4.261695146560669 sec to train the model
+"""
+"""
+GPU, ptetrained GloVe embeddings, 200 training samples. Pretrained embeddings perfor a bit better!
 Epoch 10/10
 7/7 [==============================] - 0s 49ms/step - loss: 0.0459 - acc: 1.0000 - val_loss: 0.9884 - val_acc: 0.5387
 It took 4.550364971160889 sec to train the model
 """
+"""
+GPU, self-trained embeddings, 20,000 samples
+Epoch 10/10
+625/625 [==============================] - 2s 3ms/step - loss: 1.9385e-09 - acc: 1.0000 - val_loss: 1.6083 - val_acc: 0.7840
+It took 21.027542114257812 sec to train the model
+"""
+"""
+GPU, ptetrained GloVe embeddings, 20,000 samples. Where there are enough samples pre-trained embeddings do not work well!!!
+Epoch 10/10
+625/625 [==============================] - 1s 2ms/step - loss: 0.1887 - acc: 0.9225 - val_loss: 1.0424 - val_acc: 0.6760
+It took 12.788779020309448 sec to train the model
+"""
+
+"""
+GPU, self-trained embeddings (8d), 20K train samples, 10K words 
+Epoch 10/10
+625/625 [==============================] - 6s 10ms/step - loss: 0.0372 - acc: 0.9877 - val_loss: 0.6951 - val_acc: 0.8711
+It took 64.8416862487793 sec to train the model
+"""
+
+
